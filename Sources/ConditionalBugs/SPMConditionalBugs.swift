7fcdaec4a337f5d949ce0254de5a6ecc979382c9
Strega's Gate <STREGA@users.noreply.github.com>
2023-05-18 23:20:39 -0400

    Add build action

--- Sources/ConditionalBugs/SPMConditionalBugs.swift
+++ Sources/ConditionalBugs/SPMConditionalBugs.swift
@@ -3,28 +3,40 @@
 
 #if os(Android)
 import AndroidOnly
+#warning("Imported AndroidOnly")
 #endif
 
-#if os(iOS)
+#if os(iOS) && !targetEnvironment(macCatalyst)
 import iOSOnly
+#warning("Imported iOSOnly")
 #endif
 
 #if os(Linux)
 import LinuxOnly
+#warning("Imported LinuxOnly")
+#endif
+
+#if targetEnvironment(macCatalyst)
+import MacCatalystOnly
+#warning("Imported MacCatalystOnly")
 #endif
 
 #if os(macOS)
 import MacOSOnly
+#warning("Imported MacOSOnly")
 #endif
 
 #if os(tvOS)
 import tvOSOnly
+#warning("Imported tvOnly")
 #endif
 
 #if os(WASI)
 import WASIOnly
+#warning("Imported WASIOnly")
 #endif
 
 #if os(Windows)
 import WindowsOnly
+#warning("Imported WindowsOnly")
 #endif
